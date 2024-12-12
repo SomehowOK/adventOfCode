b2bd877bfb19e51762a30f643054499139ddf1c5
jakobweber <Jakob-Weber94@web.de>
2024-12-12 16:33:42 +0100

    02. Works now

--- 02.12/02.cbl
+++ 02.12/02.cbl
@@ -4,7 +4,7 @@
        input-output section.
        file-control.
            select InputFile assign to
-            ""
+            "/Users/jakobweber/adventOfCode/02.12/input.txt"
            ORGANIZATION IS LINE SEQUENTIAL.
 
        data division.
@@ -23,12 +23,25 @@
        01 CNTR-EXTRA   PIC 9(2).
        01 CNTR-End     PIC 9(2).
        01 DEBUG1        PIC X(20).
+       01 delta         PIC S9(4).
+       01 CNTR-INPUTS   PIC 9(2).
+       01 CNTR-INPUTS-VAR PIC 9(2).
+
+       01 Number1 Pic 9(2).
+       01 Number2 Pic 9(2).
+
+       01 SAFE             PIC 9(4).
+
+       01 deltaBool       PIC X(2).
+           88 deltaBool-rising  VALUE "00".
+           88 deltaBool-declining  VALUE "10".
+           88 deltaBool-first     VALUE "99".
 
        01 InputFile-Status   PIC XX.
            88 InputFile-OK   VALUE "00".
            88 InputFile-EOF  VALUE "10".
        
-       01 TableIndex VALUE ZEROS    PIC 9999. 
+       01 TableIndex VALUE 01    PIC 9999. 
     
        01 NumberTable.
            05 Table-Data OCCURS 10 TIMES PIC 9(4).  
@@ -48,14 +61,17 @@
                    AT END 
                        Move "10" TO InputFile-Status
                    NOT AT END 
-                       ADD 1 TO TableIndex
+                       Display InputRecord
+
 
                        perform TrimString
                        perform CreateTable
+                       perform redNoseReports
                        perform InitTable
-                   
+                       
                END-READ
            end-perform
+           Display "Final: " Safe
 
            CLOSE InputFile.
        Exit. 
@@ -74,7 +90,6 @@
 
        CreateTable Section.
            MOVE 01 TO CNTR-START
-           Display InputTrim
            PERFORM VARYING CNTR-End
            
                    FROM 1 BY 1 
@@ -85,6 +100,8 @@
 
                       MOVE InputTrim(CNTR-Start:CNTR-EXTRA)
                       TO Table-Data(TableIndex)
+
+                      ADD 1 TO CNTR-INPUTS
 
                       Compute CNTR-Start = 
                               CNTR-End + 1
@@ -98,7 +115,6 @@
            MOVE InputTrim(CNTR-Start:CNTR-EXTRA)
                       TO Table-Data(TableIndex)
 
-           Display NumberTable
        Exit.
 
        InitTable Section.
@@ -110,10 +126,52 @@
                 
            end-perform
            MOVE ZEROS TO CNTR-TABLE
-           MOVE ZEROS TO TABLEINDEX
+           MOVE 01 TO TABLEINDEX
            Move 01 TO CNTR-Start
            Move ZEROS TO CNTR-END
            Move ZEROS TO CNTR-EXTRA
            MOVE ZEROS TO CNTR
+           MOVE ZEROS TO CNTR-INPUTS
+           
+       Exit.
+ 
+       redNoseReports Section.
 
+           MOVE "99" TO deltaBool
+           Move 01 TO TableIndex
+
+           Perform Until TableIndex >= (CNTR-INPUTS + 1)
+
+               COMPUTE delta = Table-Data(TableIndex) - 
+                               Table-Data(TableIndex + 1)
+               IF delta = 0 
+               THEN                                 
+                  Display "bc same"
+                  exit section
+               ENd-IF
+
+               IF deltaBool = "99"                    
+                   if delta > 0
+                   Then
+                       MOVE "10" TO deltaBool 
+                   end-if
+                   
+                   IF delta < 0
+                       MOVE "00" TO deltaBool  
+                   END-IF
+               END-IF
+           
+               IF deltaBool = "00" AND ( delta < (- 3) OR delta >= 0)
+                   Then                  
+                   exit section
+               End-If
+
+               IF deltaBool = "10" AND (delta > 3 OR delta <= 0)
+               Then
+                   exit section
+               End-If
+               Add 1 To TableIndex
+
+           End-perform
+           Add 1 To Safe
        Exit.
